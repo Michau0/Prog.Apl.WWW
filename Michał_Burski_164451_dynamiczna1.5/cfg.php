05219c817cfeb46916b57e79befa9d1f6ad195c7
Michau0 <92025674+Michau0@users.noreply.github.com>
2023-11-22 12:56:17 +0100

    lab6

---
+++ Michał_Burski_164451_dynamiczna1.5/cfg.php
@@ -0,0 +1,10 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $baza = 'mojastrona';
+
+    $link = mysql-connect($dbhost, $dbuser, $dbpass);
+    if (!$link) echo '<b>przerwane połączenie </b>';
+    if(!mysql_select_db($baza)) echo 'nie wybrano bazy';
+?>
